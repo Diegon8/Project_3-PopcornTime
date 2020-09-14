99721910847f9bf8d89050edaadfaf0fddf9d0db
Diegon8 <diegolpr95@gmail.com>
2020-09-14 22:41:12 +0200

    Argparse done

--- Cleaning.py
+++ Cleaning.py
@@ -7,8 +7,8 @@ def decader(x):
 def dollar_to_int(x):
     return x.str.replace('$','').str.replace(' ','').astype('int64')
 
-def create_table(x, y):
-    x= round((df.groupby(y).agg({'budget':['max','mean'],"worlwide_gross_income":['max','mean'],"profit":["max",'mean']}))/1000000 , 0).astype('int64')
-    x['profit_ratio']= round(df.groupby(y).agg({"profit_ratio":"mean"}),2)
-    x['n_movies']=df.groupby(y).agg({'title':'count'})
+def create_table(i,x, y):
+    x= round((i.groupby(y).agg({'budget':['max','mean'],"worlwide_gross_income":['max','mean'],"profit":["max",'mean']}))/1000000 , 0).astype('int64')
+    x['profit_ratio']= round(i.groupby(y).agg({"profit_ratio":"mean"}),2)
+    x['n_movies']=i.groupby(y).agg({'title':'count'})
     return x
---
+++ main.py
@@ -0,0 +1,35 @@
+#imports
+import numpy as np
+import pandas as pd
+import requests
+import json
+import os
+import re
+import argparse
+
+
+#Import Clean df and summary tables
+df = pd.read_csv('Input/cleanIMDB.csv',encoding = 'latin-1')
+
+dacade_table = pd.read_csv('Input/decade_table.csv',encoding = 'latin-1')
+
+score_table = pd.read_csv('Input/score_table.csv',encoding = 'latin-1')
+
+country_table = pd.read_csv('Input/country_table.csv',encoding = 'latin-1')
+
+df_str =df.astype('str')
+
+#Creating filters
+parser = argparse.ArgumentParser(description='Movie filter')
+parser.add_argument('-t','--typesearch', type=str, help='Type of search(Input: title, year, director, genre, etc.')
+parser.add_argument('-m','--match', type=str, help='Type of search must match(Input: hobbit, 2016, Peter Jackson, Action, etc')
+
+
+args=parser.parse_args()
+
+def filtro(typesearch,match):
+    return df_str[df_str[typesearch].str.contains(match,case=False)]
+
+if __name__ == '__main__':
+    print(filtro(args.typesearch, args.match))
+    
