8a092ff2c7a42ab7870693fc98267e0ade19ca88
Diegon8 <diegolpr95@gmail.com>
2020-09-15 00:18:06 +0200

    Avances API

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #imports
 import numpy as np
 import pandas as pd
-import requests
-import json
 import os
 import re
 import argparse
@@ -19,11 +17,10 @@ country_table = pd.read_csv('Input/country_table.csv',encoding = 'latin-1')
 
 df_str =df.astype('str')
 
-#Creating filters
+#Creating filtering tool 
 parser = argparse.ArgumentParser(description='Movie filter')
-parser.add_argument('-t','--typesearch', type=str, help='Type of search(Input: title, year, director, genre, etc.')
-parser.add_argument('-m','--match', type=str, help='Type of search must match(Input: hobbit, 2016, Peter Jackson, Action, etc')
-
+parser.add_argument('-t','--typesearch', type=str, metavar='', help='Type of search(Input: title, year, director, genre, etc.')
+parser.add_argument('-m','--match', type=str, metavar='', help='Type of search must match characters(Input: hobbit, 2016, Peter Jackson, Action, etc')
 
 args=parser.parse_args()
 
