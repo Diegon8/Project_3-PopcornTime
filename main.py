fd532a857653b55bcbfb73a9ecde3cd18adb6ec8
Diegon8 <diegolpr95@gmail.com>
2020-09-15 08:42:56 +0200

    Error con APIs

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ df_str =df.astype('str')
 #Creating filtering tool 
 parser = argparse.ArgumentParser(description='Movie filter')
 parser.add_argument('-t','--typesearch', type=str, metavar='', help='Type of search(Input: title, year, director, genre, etc.')
-parser.add_argument('-m','--match', type=str, metavar='', help='Type of search must match characters(Input: hobbit, 2016, Peter Jackson, Action, etc')
+parser.add_argument('-m','--match', type=str, metavar='',  help='Type of search must match characters(Input: hobbit, 2016, Peter Jackson, Action, etc')
 
 args=parser.parse_args()
 
